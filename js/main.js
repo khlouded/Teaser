b218c541b9ff57c51e5e54e4c539c8069b3f9563
khlouded <sam.khavan.tran@gmail.com>
2020-01-12 13:05:14 -0500

    Added all patterns

--- js/main.js
+++ js/main.js
@@ -9,56 +9,81 @@ function navScroll() {
         var wScroll = $(window).scrollTop(); // a variable that is a number that describes how far you have scrolled
         
         // console.log(wScroll);//This is just to check the scroll value if I need number information. I think this is based on pixels.
-
             //Visibility Logic
             //This uses breakpoints of each section. When we scroll to one section, then the display of the other section would show up or dissapear. I need to decide weather the breakpoints are based on the end or the start of a section, as if it's display: none, then that section might not exists in the DOM
 
         //Breakpoints
         //Section One
         var windowHeight = window.innerHeight;
-        var scrollTop = $('body').scrollTop();
         // For percentage of scroll I can just multiply by that value so 50% = *.5 or 90% = *.9
         
         // If the scroll position is passed the bottom of section one
         // With this method, we have the space of the windowHeight inbetween each transition. Hopefully it's not noticeable
+        // If the scroll position is passed the bottom of start (THIS IS SECTION ONE)
+
+        if (wScroll <= ($(".sec-one").position().top + $(".sec-one").innerHeight() * 0.1) ) {
+            $('.tagline p').removeClass('no-opacity');
+        } else {
+            $('.tagline p').addClass('no-opacity');
+        }
         if (wScroll <= ($(".sec-one").position().top + $(".sec-one").innerHeight()) && wScroll >= ($(".sec-one").position().top) ) {
-            $('.com-mid').css({ 
-                'transform':'translateX(' + wScroll + 'px)'// testing transform movement
-            });
-            var element1 = scrollTop - $(".sec-one").position().top;
+            $('.sec-one').addClass('visible-show');
+            var element1 = Math.abs(wScroll - $(".sec-one").position().top);
             console.log(element1 + " break one");
             // console.log(element);
         } else {
-
+            $('.sec-one').removeClass('visible-show');
         }
-
+        // If the scroll position is passed the bottom of section one (THIS IS SECTION TWO)
         if (wScroll >= ($(".sec-one").position().top + $(".sec-one").innerHeight()) && wScroll <= ($(".sec-two").position().top + $(".sec-two").innerHeight())) { //so what i'm doing is finding the position of the bottom of the first element and comparing that to the scroll value
             // console.log("Section-Two");
             $('.sec-one').removeClass('visible-show');
             $('.sec-two').addClass('visible-show');
-            var element2 = scrollTop - $(".sec-two").position().top;
+            $('.com-mid').css({ 
+                'transform':'translateX(' + wScroll/2 + 'px)'// testing transform movement
+            });
+            $('.com-bot').css({ 
+                'transform':'translateX(' + -wScroll/2 + 'px)'// testing transform movement
+            });
+            $('.com-top').css({ 
+                'transform':'translateX(' + -wScroll/2 + 'px)'// testing transform movement
+            });
+            var element2 = Math.abs(wScroll - $(".sec-two").position().top);
             console.log(element2 + " break two");
         } else {
             // console.log("Section-One");
-            $('.sec-one').addClass('visible-show');
             $('.sec-two').removeClass('visible-show');
         }
-        // If the scroll position is passed the bottom of section two
-        if (wScroll >= ($(".sec-two").position().top + $(".sec-two").innerHeight()) && wScroll <= ($(".sec-two").position().top + $(".sec-two").innerHeight()) ) {//so what i'm doing is finding the position of the bottom of the first element and comparing that to the scroll value
+        // If the scroll position is passed the bottom of section two (THIS IS SECTION THREE)
+        if (wScroll >= ($(".sec-two").position().top + $(".sec-two").innerHeight()) && wScroll <= ($(".sec-three").position().top + $(".sec-three").innerHeight()) ) {//so what i'm doing is finding the position of the bottom of the first element and comparing that to the scroll value
             // console.log("Section-Three");
             $('.sec-two').removeClass('visible-show');
             $('.sec-three').addClass('visible-show');
-            var element3 = scrollTop - $(".sec-three").position().top;
+            var element3 = Math.abs(wScroll - $(".sec-three").position().top);
             console.log(element3 + " break three");
+            //upside down
+            $('.com-mleft').css({ 
+                'transform':'translateY(' + element3*3 + 'px)  rotate(180deg)'// testing transform movement
+            });
+            $('.com-right').css({ 
+                'transform':'translateY(' + element3*3 + 'px)  rotate(180deg)'// testing transform movement
+            });
+            $('.com-left').css({ 
+                'transform':'translateY(' + -element3 + 'px)'// testing transform movement
+            });
+            $('.com-mright').css({ 
+                'transform':'translateY(' + -element3 + 'px)'// testing transform movement
+            });
         } else {
             $('.sec-three').removeClass('visible-show');
         }
-        //If the scroll position is passed the bottom of section three
-        if (wScroll >= ($(".sec-three").position().top + $(".sec-three").innerHeight()) ) {//so what i'm doing is finding the position of the bottom of the first element and comparing that to the scroll value
+        //If the scroll position is passed the bottom of section three (THIS IS SECTION FOUR)
+        if (wScroll >= ($(".sec-three").position().top + $(".sec-three").innerHeight()) && wScroll <= ($(".sec-final").position().top + $(".sec-final").innerHeight())) {//so what i'm doing is finding the position of the bottom of the first element and comparing that to the scroll value
             // console.log("Section-Three");
+            
             $('.sec-three').removeClass('visible-show');
             $('.sec-final').addClass('visible-show');
-            var element4 = scrollTop - $(".sec-final").position().top;
+            var element4 = Math.abs(wScroll - $(".sec-final").position().top);
             console.log(element4  + " break four");
         } else {
             $('.sec-final').removeClass('visible-show');
