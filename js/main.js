02f10de8deecf6fc37bdfac57c1437626355baa7
khlouded <sam.khavan.tran@gmail.com>
2020-02-12 23:48:41 -0500

    changed helvetica

--- js/main.js
+++ js/main.js
@@ -229,16 +229,16 @@ function ranFont() {
     // Helvetica Neue TESTING HERE CHANGE AFTER
 
     $(".variable span").each(function(){
-        var helvetica = "Helvetica"; // check to see what it is actually called once we get the fonts
+        var helvetica = 'Helvetica URL'; // check to see what it is actually called once we get the fonts
         var parabole = 'Parabole Regular';
         var fonts = [helvetica, parabole]; //an array with the different possible fonts
         var randomFont = fonts[Math.floor(Math.random()*fonts.length)]; //This spits out a random choice between helvetica and parabole
         var currentElement = $(this); //this targets every current element, and iterates it from the first to the last. Since we used lettery
         var size = currentElement.css("font-size"); // this gets the size of the element so that we can manipulate it later
         // console.log(size);
-        currentElement.css("font-family", randomFont + ", sans serif");//This sets the css property with the randomFont. The bread and butter of the function
+        currentElement.css("font-family", randomFont + ", \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif");//This sets the css property with the randomFont. The bread and butter of the function
 
-        if (currentElement.css("font-family") == "\"Helvetica Neue Lt Std\", \"sans-serif\"" || currentElement.css("font-family") == "\"Helvetica Neue\", \"sans-serif\"" ||currentElement.css("font-family") == "\"Helvetica Neue\", sans serif" || currentElement.css("font-family") == "Helvetica Neue, sans-serif" || currentElement.css("font-family") == "Helvetica, sans-serif" || currentElement.css("font-family") == "\"Helvetica\", sans-serif"  || currentElement.css("font-family") == "\'Helvetica\', sans-serif" ) {
+        if (currentElement.css("font-family") == "\"Helvetica Neue Lt Std\",\"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", \"sans-serif\"" || currentElement.css("font-family") == "\"Helvetica Neue\",\"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", \"sans-serif\"" ||currentElement.css("font-family") == "\"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\",  sans serif" || currentElement.css("font-family") == "\"Helvetica Neue\", \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\",  sans-serif" || currentElement.css("font-family") == "Helvetica, \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif" || currentElement.css("font-family") == "\"Helvetica\", \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif"  || currentElement.css("font-family") == "\"Helvetica Neue\", \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif"  || currentElement.css("font-family") == "\"HelveticaNeue-Light\", \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\",sans-serif"  || currentElement.css("font-family") == "\'Helvetica\', \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif" || currentElement.css("font-family") == "\"Helvetica Neue Lt Std\", \"sans-serif\"" || currentElement.css("font-family") == "\"Helvetica Neue\",   \"sans-serif\"" ||currentElement.css("font-family") == "\"Helvetica Neue\",  sans serif" || currentElement.css("font-family") == "\"Helvetica Neue\",  sans-serif" || currentElement.css("font-family") == "Helvetica, sans-serif" || currentElement.css("font-family") == "\"Helvetica\",  sans-serif"  || currentElement.css("font-family") == "\'Helvetica\',  sans-serif" || currentElement.css("font-family") == "\'Helvetica URL\', \"Helvetica Neue\", \"Helvetica Neue Light\", \"HelveticaNeue-Light\", \"Helvetica URL\", Helvetica, \"open sans\", sans-serif" ) {
             // console.log("Helvetica");
             currentElement.css({'font-size':"calc( " + size + " * 0.98)"});
         } else {
@@ -494,23 +494,38 @@ function customCursor() {
 }
 function macResponse() {
 
-var mac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+    var mac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
 
-if (mac) {
-    $('.pat-one .comp h1').css({
-        'letter-spacing': '0',
-        '-webkit-letter-spacing': '0vw'
-    });		
-} else {
-    $('.pat-one .comp h1').css({
-        'letter-spacing': '-15vw',
-        '-webkit-letter-spacing': '-15vw'
-    });		
+    if (mac) {
+        $('.pat-one .variable span').css({
+            'margin': '-3vw',
+        });		
+        $('.pat-three .variable span').css({
+            'margin': '-0vw',
+        });		
+    } else {
+        $('.pat-one .variable span').css({
+            'margin': '-10vw',
+        });		
+        $('.pat-three .variable span').css({
+            'margin': '-5vw',
+        });		
+    }
+    var isMobile = false; //initiate as false
+        // device detection https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device
+
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        isMobile = true;
+        $(".cursor--small").css({
+            'opacity' : '0'
+        })
+        
+    }
 }
-}
+
 //The reason why we set everything as a function and used these is to ensure that we check the scroll position before we load the page, in order to load the transformations accordingly
 $(window).scroll(function() {
-    customCursor()
+    customCursor();
     navScroll();
     navOpen();
 });
@@ -522,13 +537,13 @@ $(document).ready(function() {
     strokeFont();
     ranFont();
     ranColor();
-    favIcon()
-    detectIEEdge()
-
+    favIcon();
+    detectIEEdge();
+    macResponse();
 });
 // this allows for resize changes, so that we can redu all of the items
 $(window).resize(function() {
-    customCursor()
+    customCursor();
     navScroll();
     navOpen();
     strokeFont();
